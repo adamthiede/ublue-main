8d863ea06498001ae86509981639dfc1bc62a1ee
adamthiede <me@adamthiede.com>
2024-11-17 06:28:01 -0600

    disable nonexistent service

--- post-install.sh
+++ post-install.sh
@@ -7,13 +7,11 @@ if [[ "$IMAGE_NAME" == "base" ]]; then
 fi
 
 systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
+#systemctl enable flatpak-system-update.timer
 
-systemctl --global enable flatpak-user-update.timer
+#systemctl --global enable flatpak-user-update.timer
 
 cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
 
-ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk" 
+ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk"
 
-rm -f /etc/yum.repos.d/_copr_ublue-os_staging.repo
-rm -f /etc/yum.repos.d/_copr_kylegospo_oversteer.repo
